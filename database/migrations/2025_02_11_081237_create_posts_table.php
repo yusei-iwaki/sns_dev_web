d38569484cf51e07a98a316cca58f1080602f8f4
yusei-iwaki <iwaki.yusei11@gmail.com>
2025-02-11 17:40:33 +0900

    マイグレーション作成
    投稿テーブル

--- database/migrations/2025_02_11_081237_create_posts_table.php
+++ database/migrations/2025_02_11_081237_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->text('content');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
